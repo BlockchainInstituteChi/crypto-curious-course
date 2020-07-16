3f5ceb8d406500d466b832069a87a5f472463b32
al <alex@theblockchaininstitute.org>
2020-07-16 14:31:28 -0500

    fixing presenter notes and adding toc support + title

--- assets/js/broadcastLayer.js
+++ assets/js/broadcastLayer.js
@@ -1,32 +1,24 @@
-// use local storage for messaging. Set message in local storage and clear it right away
-// This is a safe way how to communicate with other tabs while not leaving any traces
-//
 var bc = new BroadcastChannel('test_channel');
 
 function message_broadcast(message)
 {   
-    // var bc = new BroadcastChannel('test_channel');
+    console.log('sending', message)
     bc.postMessage(message); 
-    // console.log('sending message', message)
-    // localStorage.setItem('message',JSON.stringify(message));
-    // localStorage.removeItem('message');
 }
 
-
-// receive message
-//
-function message_receive(ev)
-{
+bc.onmessage = function (ev) { 
     console.log('message received', ev)
-    if (ev.originalEvent.key!='message') return; // ignore other keys
-    var message=JSON.parse(ev.originalEvent.newValue);
-    if (!message) return; // ignore empty msg or msg reset
-
-    // here you act on messages.
-    // you can send objects like { 'command': 'doit', 'data': 'abcd' }
-    if (message.command == 'doit') alert(message.data);
-
-    // etc.
+    window.onMessageEvent(ev)
 }
 
-bc.onmessage = function (ev) { console.log('msg received', ev); }
+// Note: Must provide function 'onMessageEvent' to handle received messages
+
+// Template: 
+// 
+// window.onMessageEvent = function ( ev ) {
+//     console.log('handling event', ev)
+//     if ( event.trigger === "test" ) {
+//         console.log('test trigger!')
+//     }   
+// }
+// 
